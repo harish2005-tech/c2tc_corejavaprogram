41057d668bb8b3704d937a1190a95ed8b050f5a9
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-NQ3CE08>
2025-07-21 23:00:58 -0700

    program

---
+++ C2tccorejavaprogram.java/src/Day1/java/FirstProgram.java
@@ -0,0 +1,11 @@
+package Day1.java;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+			System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
