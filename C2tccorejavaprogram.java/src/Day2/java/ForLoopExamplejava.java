f9f64e0d917264fe4f89b867269cc5ae4a1618ce
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-NQ3CE08>
2025-07-22 21:15:13 -0700

    program

---
+++ C2tccorejavaprogram.java/src/Day2/java/ForLoopExamplejava.java
@@ -0,0 +1,13 @@
+package Day2.java;
+
+public class ForLoopExamplejava {
+	public static void main(String[] args) 
+	{	
+		for(int i = 1; i<=1000 ; i++)   //(int i = 1000; i<=1;i--)
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+	}
+
+}
