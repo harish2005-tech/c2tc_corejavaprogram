409f4fcc42a63b9c8dc11654954bbda84870a551
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-NQ3CE08>
2025-07-25 03:13:47 -0700

    pro

---
+++ C2tccorejavaprogram.java/src/Day6/staticvariable/EmployeeDemo.java
@@ -0,0 +1,15 @@
+package Day6.staticvariable;
+
+public class EmployeeDemo {
+public static void main(String[] args) {
+		
+		System.out.println(Employee.companyName);
+		Employee e = new Employee("Maha", 101);
+		System.out.println(e);
+		
+		
+		e = new Employee("Deepika", 102);
+		System.out.println(e);
+	}
+
+}
